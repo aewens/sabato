2aff08164481b1043f6fc411ea5c7bdb870cbce6
aewens <email@aewens.com>
2020-04-18 18:20:11 -0500

    Finished echo server

--- socket-20200418/Program.cs
+++ socket-20200418/Program.cs
@@ -23,17 +23,41 @@ namespace socket_20200418
                 listener.Bind(localEndPoint);
                 listener.Listen(10);
 
+                Console.WriteLine("Waiting for a connection...");
+                Socket handler = listener.Accept();
+
                 while (true) {
-                    Console.WriteLine("Waiting for a connection...");
+                    data = null;
+
+                    while (true) {
+                        int bytesRec = handler.Receive(bytes);
+                        data = data + Encoding.ASCII.GetString(bytes, 0,
+                            bytesRec);
+
+                        Console.Write(data);
+                        if (data.IndexOf(".") > -1) {
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine("Received: {0}", data);
+
+                    byte[] message = Encoding.ASCII.GetBytes(data);
+                    handler.Send(message);
+                    //handler.Shutdown(SocketShutdown.Both);
+                    //handler.Close();
                 }
             } catch (Exception e) {
                 Console.WriteLine(e.ToString());
             }
+
+            Console.WriteLine("\nPress <Enter> to continue\n");
+            Console.Read();
         }
 
         public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartListening();
             return 0;
         }
     }
