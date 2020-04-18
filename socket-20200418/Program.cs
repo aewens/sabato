25dba43d32a0c8f9835f5111a7ddaf27f27d84f9
aewens <email@aewens.com>
2020-04-18 18:03:45 -0500

    Added start to event loop and preamble for network data

--- socket-20200418/Program.cs
+++ socket-20200418/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace socket_20200418
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string data = null;
+
+        public static void StartListening() {
+            byte[] bytes = new Byte[1024];
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 20202);
+
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            try {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true) {
+                    Console.WriteLine("Waiting for a connection...");
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static int Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            return 0;
         }
     }
 }
